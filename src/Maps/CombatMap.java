922df96f37a7daa289c782448e31610363567891
Chris Anzilotti <anzilottichr@gmail.com>
2023-09-27 16:06:22 -0400

    Fixed Tileset issues

--- src/Maps/CombatMap.java
+++ src/Maps/CombatMap.java
@@ -1,14 +1,15 @@
 package Maps;
 
 import Level.Map;
-import Tilesets.CommonTileset;
+//import Tilesets.QuadTileset;
+import Tilesets.TestTileset;
 import Utils.Point;
 
 public class CombatMap extends Map 
 {
     public CombatMap()
     {
-        super("combat_screen.txt", new CommonTileset());
+        super("combat_screen.txt", new TestTileset());
         this.playerStartPosition = new Point(1, 11);
     }
 }
