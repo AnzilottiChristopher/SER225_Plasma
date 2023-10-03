8ebc139ec3939c612a36ff2b7a310f3a603033fe
Chris Anzilotti <anzilottichr@gmail.com>
2023-10-03 17:19:01 -0400

    Initial Commit

---
+++ src/EnhancedMapTiles/OutOfBoundsCollision.java
@@ -0,0 +1,14 @@
+package EnhancedMapTiles;
+
+import GameObject.GameObject;
+import Level.EnhancedMapTile;
+import Level.TileType;
+
+public class OutOfBoundsCollision extends EnhancedMapTile
+{
+
+    public OutOfBoundsCollision(float x, float y, GameObject bottomLayer, GameObject.GameObject topLayer, TileType tileType)
+    {
+        //super(x, y, bottomLayer, topLayer, tileType);
+    }
+}
