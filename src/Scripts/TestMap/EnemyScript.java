be0069bcf464ffa7665d4bf8f85e8175d7634e8e
Lucas Jenkins <lgjenkins@quinnipiac.edu>
2023-10-25 23:22:32 -0400

    Added audio for robots

--- src/Scripts/TestMap/EnemyScript.java
+++ src/Scripts/TestMap/EnemyScript.java
@@ -1,22 +1,32 @@
 package Scripts.TestMap;
 
+import Engine.Music;
 import Level.NPC;
 import Level.Script;
 import Level.ScriptState;
 
 public class EnemyScript extends Script<NPC>
 {
+
+    Music music = new Music();
     @Override
     protected void setup()
     {
         entity.facePlayer(player);
         lockPlayer();
         showTextbox();
+        setFlag("hasTalked");
         if (entity.getId() == 6)
         {
+            music.stopLoop();
+            music.background("Resources/Announce the truth.wav");
+            music.playLoop();
             addTextToTextboxQueue("01001000 01001000 01001000 01010000");
         } else if (entity.getId() == 7)
         {
+            music.stopLoop();
+            music.background("Resources/Announce the truth.wav");
+            music.playLoop();
             addTextToTextboxQueue("P...lease");
             addTextToTextboxQueue("Must finish....");
             addTextToTextboxQueue("Game...Almost");
@@ -29,6 +39,8 @@ public class EnemyScript extends Script<NPC>
     {
         unlockPlayer();
         hideTextbox();
+        setFlag("startingMusic");
+        music.stopLoop();
     }
 
     @Override
